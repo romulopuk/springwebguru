319ab0d499de2eb0e76ff79e3f5464cb34b849c9
Romulo Motta <pukpen@gmail.com>
2021-04-05 20:03:02 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/springwebguru/SpringwebguruApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebguruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebguruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springwebguru/SpringwebguruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebguruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
